72d86ece4c93f4cca9d2f86a7c6d70e651d7bdb1
BlazeTurk05 <49597666+BlazeTurk05@users.noreply.github.com>
2019-07-23 17:23:20 -0500

    Add files via upload

---
+++ queries (1).sql
@@ -0,0 +1,47 @@
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM salaries AS s
+INNER JOIN employees AS e ON
+e.emp_no = s.emp_no;
+
+SELECT * FROM employees
+WHERE hire_date LIKE '1986%';
+
+SELECT d.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name, m.from_date, m.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS m ON
+m.dept_no = d.dept_no
+JOIN employees AS e ON
+e.emp_no = m.emp_no;
+
+SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS dp ON
+dp.dept_no = d.dept_no;
+
+SELECT * FROM employees
+WHERE first_name LIKE 'Hercules'
+AND last_name LIKE 'B%';
+
+SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS dp ON
+dp.dept_no = d.dept_no
+WHERE dp.dept_name LIKE 'Sales';
+
+SELECT e.emp_no, e.last_name, e.first_name, dp.dept_name
+FROM employees AS e
+INNER JOIN dept_emp AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS dp ON
+dp.dept_no = d.dept_no
+WHERE dp.dept_name LIKE 'Development'
+OR dp.dept_name LIKE 'Sales';
+
+SELECT last_name, COUNT(*) AS frequency
+FROM employees
+GROUP BY last_name
+ORDER BY frequency DESC;
